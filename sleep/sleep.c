50c83710f6412ae557247d5273d7b8729b3ff8be
lys94666 <aa91824026@gmail.com>
2023-07-13 10:28:33 +0800

    sleep.c

--- sleep/sleep.c
+++ sleep/sleep.c
@@ -1,1 +1,20 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/fcntl.h"
 
+int 
+main(int argc,char **argv)
+{ 
+    if(argc != 2)
+    {
+         fprintf(2,"error");
+         exit(1);
+    }
+    else
+    {
+        int n = atoi(argv[1]);
+        sleep(n);
+        exit(0);
+    }
+    exit(0);
+}
